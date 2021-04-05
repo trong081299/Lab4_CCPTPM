8770cba5dff366ee63e2e1551b67429a92d15813
“trong081299” <nguyendinhtrong081299@gmail.com>
2021-04-05 15:30:14 +0700

    Dinh Trong update lan 4

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -14,6 +14,7 @@ namespace lab4
         [STAThread]
         static void Main()
         { //update
+            //update lan 2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
